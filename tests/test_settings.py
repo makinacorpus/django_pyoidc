0cf4a46a79a8a6453954f723a83c4c1a17203ee7
Paul Florence <paul.florence@makina-corpus.com>
2023-03-27 13:30:04 +0200

    Test oidc views

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,7 +1,13 @@
 from decouple import config
 
 SECRET_KEY = "fake-key"
-INSTALLED_APPS = ["tests", "makina_django_oidc"]
+INSTALLED_APPS = ["django.contrib.sessions", "tests", "makina_django_oidc"]
+
+
+MIDDLEWARE = [
+    "django.contrib.sessions.middleware.SessionMiddleware",
+]
+
 
 DATABASES = {
     "default": {
@@ -13,3 +19,22 @@ DATABASES = {
         "PORT": config("POSTGRES_PORT", default=5432),
     }
 }
+MAKINA_DJANGO_OIDC = {
+    "test": {
+        "PROVIDER_URI": "http://oidc.test/",
+        "REDIRECT_URI": "http://oidc.test/callback",
+        "CLIENT_SECRET": "EnSAdFDlM78HejQ5EQATtlvXgRzfNww4",
+        "CLIENT_ID": "full",
+        "CONFIG_URI": "/auth/realms/Demo",
+        "REDIRECT_FAILURE_URI": "http://oidc.test/",
+        "REDIRECT_LOGOUT_URI": "http://oidc.test/",
+        "REDIRECT_SUCCESS_DEFAULT_URI": "http://oidc.test/",
+        "REDIRECT_REQUIRES_HTTPS": False,
+        "REDIRECT_ALLOWED_HOSTS": ["oidc.test"],
+        "SCOPE": "full-dedicated",
+        "CACHE_BACKEND": "default",
+    }
+}
+
+
+ROOT_URLCONF = "tests.urls"
--- tests/tests_jwt.py
+++ tests/tests_jwt.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class JwtValidationTestCase(TestCase):
-    def test_expired_jwt_is_rejected(self):
-        pass
+    def test_expired_token_is_rejected(self):
+        self.client.get(reverse("test_login"))
+        self.client.get(reverse("test_callback"))
