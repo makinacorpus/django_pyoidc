7ca68a79e153750d96dcdc4afe5f87bc3b82ee2c
Paul Florence <paul.florence@makina-corpus.com>
2023-05-26 09:48:03 +0200

    Cleanup e2e code from main source folder

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -2,9 +2,6 @@ import os
 
 from decouple import config
 
-PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.path.dirname(PROJECT_DIR)
-
 SECRET_KEY = "fake-key"
 INSTALLED_APPS = [
     "django.contrib.contenttypes",
@@ -20,22 +17,6 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
-]
-
-TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(PROJECT_DIR, "templates")],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-            ],
-        },
-    },
 ]
 
 # SESSION_ENGINE = "django.contrib.sessions.backends.file"
