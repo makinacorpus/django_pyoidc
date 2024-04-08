ea35b770af840b263cd92d5e1df5ce9d9c23f174
regilero <regis.leroy@makina-corpus.com>
2024-04-08 15:30:52 +0200

    Fix: Better get_setting_for_sso_op

--- django_pyoidc/utils.py
+++ django_pyoidc/utils.py
@@ -2,9 +2,10 @@ from django.conf import settings
 
 
 def get_setting_for_sso_op(op_name: str, key: str, default=None):
-    if default and key not in settings.DJANGO_PYOIDC[op_name]:
+    if key in settings.DJANGO_PYOIDC[op_name]:
+        return settings.DJANGO_PYOIDC[op_name][key]
+    else:
         return default
-    return settings.DJANGO_PYOIDC[op_name][key]
 
 
 def get_settings_for_sso_op(op_name: str):
